e96b01b66794f88e7e29fb673740dfa09e77524c
yyttt328-star <yyttt328@gmail.com>
2026-04-23 18:04:10 +0900

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,22 +1,33 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradeOfferList;
 
 public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitialize() {
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+        ServerTickEvents.END_SERVER_TICK.register(server -> {
 
-		LOGGER.info("Hello Fabric world!");
+            for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
+
+                if (player.currentScreenHandler == null) continue;
+
+                TradeOfferList trades = player.currentScreenHandler.getRecipes();
+
+                if (trades == null) continue;
+
+                for (TradeOffer offer : trades) {
+
+                    // 💀 semua trade jadi 1 emerald
+                    offer.getFirstBuyItem().setCount(1);
+                }
+            }
+        });
+    }
+}
