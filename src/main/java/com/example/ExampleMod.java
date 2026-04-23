da7080ae6b1af5b4fe975cae44cf3d1ac0a473e7
yyttt328-star <yyttt328@gmail.com>
2026-04-24 07:12:10 +0900

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,33 +1,11 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.village.TradeOffer;
-import net.minecraft.village.TradeOfferList;
 
 public class ExampleMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-
-        ServerTickEvents.END_SERVER_TICK.register(server -> {
-
-            for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
-
-                if (player.currentScreenHandler == null) continue;
-
-                TradeOfferList trades = player.currentScreenHandler.getRecipes();
-
-                if (trades == null) continue;
-
-                for (TradeOffer offer : trades) {
-
-                    // 💀 semua trade jadi 1 emerald
-                    offer.getFirstBuyItem().setCount(1);
-                }
-            }
-        });
+        System.out.println("MOD JALAN");
     }
-} 
+}
