3143da105e24305d8418bf59c1a03967cf741606
yyttt328-star <yyttt328@gmail.com>
2026-04-24 10:08:30 +0900

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,11 +1,28 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
+
+import net.minecraft.item.Items;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.VillagerProfession;
 
 public class ExampleMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        System.out.println("MOD JALAN");
+
+        // Contoh: semua farmer jual diamond cuma 1 emerald
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.FARMER, 1, factories -> {
+            factories.add((entity, random) -> 
+                new TradeOffer(
+                    new net.minecraft.item.ItemStack(Items.EMERALD, 1), // harga
+                    new net.minecraft.item.ItemStack(Items.DIAMOND, 1), // hasil
+                    999, // max use
+                    1,   // xp
+                    0.05f
+                )
+            );
+        });
     }
 }
