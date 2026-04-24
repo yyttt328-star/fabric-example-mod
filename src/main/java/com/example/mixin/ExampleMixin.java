ab781d9e0c5ea9021ff973591fd7e4215d42b173
yyttt328-star <yyttt328@gmail.com>
2026-04-24 13:51:41 +0900

    Update ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,31 @@
 package com.example.mixin;
 
-import net.minecraft.server.MinecraftServer;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.world.World;
 
-@Mixin(MinecraftServer.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
-	}
-}
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(ZombieEntity.class)
+public class ZombieMixin {
+
+    /**
+     * @author you
+     * @reason Force 100% villager zombification
+     */
+    @Overwrite
+    public boolean tryAttack(net.minecraft.entity.Entity target) {
+        if (target instanceof VillagerEntity villager) {
+
+            World world = villager.getWorld();
+
+            if (!world.isClient) {
+                villager.convertTo(EntityType.ZOMBIE_VILLAGER, true);
+            }
+        }
+
+        return true;
+    }
+		}
